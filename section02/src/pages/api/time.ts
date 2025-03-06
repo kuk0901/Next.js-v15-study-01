cac93e3f5d8cd0f39854157d2a9fef2e240fe4a7
Dobby <kukhahyeon@gmail.com>
2025-03-06 22:31:47 +0900

    section02 패키지 삭제후 재생성

---
+++ section02/src/pages/api/time.ts
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const date = new Date();
+
+  res.json({ date: date.toLocaleString() });
+}
